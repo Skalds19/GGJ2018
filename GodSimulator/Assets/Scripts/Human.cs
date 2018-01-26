12fa23cc164c628945f04a139585153336e9751c
BERKE KAGAN NOHUT <e2036135@ceng.metu.edu.tr>
2018-01-26 23:00:17 +0300

    Tribe Classes

---
+++ GodSimulator/Assets/Scripts/Human.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : MonoBehaviour
+{
+
+    public float hunger  = 100;
+    public float thirst  = 100;
+    public float fatigue = 100;
+    public float cold    = 100;
+    public float joy     = 100;
+
+    void Start ()
+    {
+        StartCoroutine("live");
+    }
+	
+	
+	void Update ()
+    {
+		
+	}
+
+    IEnumerator live()
+    {
+        yield return new WaitForSeconds(1f);
+        hunger -= 1;
+        thirst -= 1;
+        fatigue -= 1;
+        cold -= 1;
+        joy -= 1;
+        if ( hunger > 0 && thirst > 0 && fatigue > 0 && cold > 0 && joy > 0 )
+        {
+            StartCoroutine("live");
+        }
+        else
+        {
+            die();
+        }
+    }
+    void die()
+    {
+        DestroyObject(gameObject);
+    }
+}
---
+++ GodSimulator/Assets/Scripts/Tribe.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tribe : MonoBehaviour {
+
+    public GameObject tribeMember;
+    public GameObject spawnLocation;
+
+    public int resourceType;
+
+    private int people = 2;
+    private int food = 0;
+    private int water = 0;
+    private int wool = 0;
+    private int tool = 0;
+
+    public void changeFoodSupply( int changeAmount )
+    {
+        food += changeAmount;
+    }
+    public void changeWaterSupply( int changeAmount )
+    {
+        water += changeAmount;
+    }
+    public void changeWoolSupply( int changeAmount )
+    {
+        wool += changeAmount;
+    }
+    public void changeToolSupply( int changeAmount )
+    {
+        tool += changeAmount;
+    }
+
+    public int getFood()
+    {
+        return food;
+    }
+    public int getWater()
+    {
+        return water;
+    }
+    public int getWool()
+    {
+        return wool;
+    }
+    public int getTool()
+    {
+        return tool;
+    }
+
+    public void increasePopulation()
+    {
+        people += 1;
+        Instantiate(tribeMember, spawnLocation.transform.position, spawnLocation.transform.rotation, spawnLocation.transform);
+    }
+    private void OnMouseDown()
+    {   
+        if( food >= 30 && water >= 30 && wool >= 30 )
+        {
+            food -= 30;
+            water -= 30;
+            wool -= 30;
+            increasePopulation();
+        }
+        
+    }
+    
+}
